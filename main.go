87872088862d606e2a8719436674a79298401710
MeguruForever <1401080341@qq.com>
2023-03-09 00:07:19 +0800

    calculator

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ func main(){
 	fmt.Println("输入算式")
 	var str string
 	fmt.Scan(&str)
+	str = src.Match(str)
+	fmt.Println(str)
 	fmt.Println(src.Calculate(str))
 	fmt.Println("输入任意键退出")
 	var s string
